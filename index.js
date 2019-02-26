5fa22552606025fd31fdd933377c07d552c0bdd6
Saiteja-vadlapatla <saitejavadlapatla@gmail.com>
2019-02-26 16:24:06 +0530

    Final changes - slightly improved UI

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let root = document.getElementById('root');
 let cellColor = 'white';
 let apply = false;
-const ROWS = 10;
-const COLS = 10;
+const ROWS = 30;
+const COLS = 30;
 const PALETTE = ['red', 'green', 'blue', 'yellow', 'pink'];
 
 handleclick = (e) => {
@@ -32,31 +32,36 @@ for (let i = 1; i < ROWS+1; i++) {
 }
 
 let palette = document.getElementById('palette');
+palette.style.display = 'flex';
 let paletterow = document.createElement('div');
 paletterow.className='paletterows';
 palette.appendChild(paletterow);
 
+
+
+let inputColor = document.createElement('input');
+//inputColor.onchange = (e) => {cellColor = e.target.value};
+inputColor.placeholder = 'Enter color ...';
+inputColor.onkeyup = (e) => {cellColor = e.target.value; colorbox.style.backgroundColor = e.target.value};
+paletterow.appendChild(inputColor)
+
+PALETTE.forEach((color) => {
+    let paletteCell = document.createElement('div');
+    paletteCell.className = 'paletteCell';
+    paletteCell.style.backgroundColor = color;
+    paletteCell.onclick = () => {cellColor = color; colorbox.style.backgroundColor = color; inputColor.value = color};
+    paletterow.appendChild(paletteCell);
+});
+
 let currentColor = document.createElement('div');
 currentColor.className = 'rows';
+currentColor.style.marginLeft = '20px';
 currentColor.innerHTML = 'Current Color --->  ';
 let colorbox = document.createElement('div');
 colorbox.className = 'colorbox';
 colorbox.style.backgroundColor = cellColor;
 currentColor.appendChild(colorbox);
 palette.appendChild(currentColor);
-
-PALETTE.forEach((color) => {
-    let paletteCell = document.createElement('div');
-    paletteCell.className = 'paletteCell';
-    paletteCell.style.backgroundColor = color;
-    paletteCell.onclick = () => {cellColor = color; colorbox.style.backgroundColor = color};
-    paletterow.appendChild(paletteCell);
-});
-
-let inputColor = document.createElement('input');
-//inputColor.onchange = (e) => {cellColor = e.target.value};
-inputColor.onkeyup = (e) => {cellColor = e.target.value; colorbox.style.backgroundColor = e.target.value};
-paletterow.appendChild(inputColor)
 
 
 
